15737c69480529f5d44c60c1c9bc42ede42b141b
Lunahey <55185494+Lunahey@users.noreply.github.com>
2022-02-10 09:55:28 +0100

    Exercice boucle

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 public class Launcher{
     public static void main(String[] args){
         System.out.println("Bienvenue");
-        Scanner sc = new Scanner(System.in);
-        String texte = sc.nextLine();
-        if (texte != "quit"){
-            System.out.println("Unknown command");
+        while(true){
+            Scanner sc = new Scanner(System.in);
+            String texte = sc.nextLine();
+            if (texte.contains("quit")){
+               break;
+            }
+            else{
+                System.out.println("Unknown command");
+            }
         }
     }
 }
